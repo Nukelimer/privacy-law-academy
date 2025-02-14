33f8d51a5ceeda059f0f7ceedfa05eb0431fb664
Nukelimer <codewithremi@gmail.com>
2025-02-14 21:25:26 +0100

    restructured the sanity structure

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,23 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { studentType } from './studentType'
+import { courseType } from './courseType'
+import { instructorType } from './instructorType'
+import { moduleType } from './moduleType'
+import { lessonType } from './lessonType'
+import { blockContent } from './blockContent'
+import { enrollmentType } from './enrollmentType'
+import { categoryType } from './categoryType'
+import { lessonCompletionType } from './lessonCompleted'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    courseType,
+    moduleType,
+    lessonType,
+    instructorType,
+    blockContent,
+    studentType,
+    enrollmentType,
+    categoryType,
+    lessonCompletionType,],
 }
